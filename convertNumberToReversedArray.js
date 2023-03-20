cb98ed3dd56cde2383673e900ca76209d7113672
leahharrison <leah.renee.harrison@gmail.com>
2023-03-20 11:25:15 -0500

    Delete convertNumberToReversedArray.js

--- convertNumberToReversedArray.js
+++
@@ -1,4 +0,0 @@
-//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-function digitize(n) {
-    return Array.from(String(n), Number).reverse();
-}
